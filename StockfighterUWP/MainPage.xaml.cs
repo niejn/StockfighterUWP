eeb65e8f4032c341cb17f481d89777670f56674c
Pinghao Qi <feifeiqph@gmail.com>
2016-07-17 23:17:29 -0400

    Initial commit, still trying out the UI design tools.

--- StockfighterUWP/MainPage.xaml.cs
+++ StockfighterUWP/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +23,16 @@ namespace StockfighterUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private TraderModel _trader;
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void startTraderButton_Click(object sender, RoutedEventArgs e)
+        {
+            _trader = new TraderModel();
+            await Task.Run(async () => await _trader.Init());
         }
     }
 }
---
+++ StockfighterUWP/TraderModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Windows.Devices.Geolocation;
+using System.Diagnostics;
+
+namespace StockfighterUWP
+{
+    class TraderModel
+    {
+        private HttpClient _client;
+
+        private const string ApiKey = "4f7c537ce4b923b340a285c8c8b3431275934937";
+
+        private string _venue;
+
+        private string _account;
+
+        private const string ApiUrl = "https://api.stockfighter.io/ob/api/";
+
+        public TraderModel(string venue = null, string account = null)
+        {
+            _client = new HttpClient();
+
+            if (venue != null)
+            {
+                _venue = venue;
+            }
+            if (account != null)
+            {
+                _account = account;
+            }
+            
+        }
+
+        public async Task Init()
+        {
+            if (await ApiIsUp())
+            {
+                Debug.WriteLine("Stockfighter API is up.");
+            }
+        }
+
+        private async Task<bool> ApiIsUp()
+        {
+            var response = await _client.GetAsync($"{ApiUrl}heartbeat");
+            return false;
+        }
+
+
+    }
+}
